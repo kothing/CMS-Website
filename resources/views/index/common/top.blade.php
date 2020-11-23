ba569e1bf6e7b4d7062f51b7db3f0861c18c3907
Kothing <39289309+kothing@users.noreply.github.com>
2020-11-23 18:56:57 +0800

    Update top.blade.php

--- resources/views/index/common/top.blade.php
+++ resources/views/index/common/top.blade.php
@@ -24,6 +24,8 @@
           baseUrl: "{{asset('static/index')}}"
       });
     </script>
+    <!--Site css-->
+    <link href="{{asset('static/index')}}/assets/css/site.css" rel="stylesheet" />
     <!-- Dashboard Core -->
     <link href="{{asset('static/index')}}/assets/css/dashboard.css" rel="stylesheet" />
     <script src="{{asset('static/index')}}/assets/js/dashboard.js"></script>
@@ -53,42 +55,6 @@
                 <img src="{{asset('uploads')}}/{{ $sys_info->web_logo }}" class="header-brand-img" alt="tabler logo">
               </a>
               <div class="d-flex order-lg-2 ml-auto">
-                <!-- <div class="nav-item d-none d-md-flex">
-                  <a href="https://github.com/tabler/tabler" class="btn btn-sm btn-outline-primary" target="_blank">广告按钮</a>
-                </div> -->
-                <!-- 消息通知 -->
-                <!-- <div class="dropdown d-none d-md-flex">
-                  <a class="nav-link icon" data-toggle="dropdown">
-                    <i class="fe fe-bell"></i>
-                    <span class="nav-unread"></span>
-                  </a>
-                  <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                    <a href="#" class="dropdown-item d-flex">
-                      <span class="avatar mr-3 align-self-center" style="background-image: url({{asset('static/index')}}/demo/faces/male/41.jpg)"></span>
-                      <div>
-                        <strong>Nathan</strong> pushed new commit: Fix page load performance issue.
-                        <div class="small text-muted">10 minutes ago</div>
-                      </div>
-                    </a>
-                    <a href="#" class="dropdown-item d-flex">
-                      <span class="avatar mr-3 align-self-center" style="background-image: url({{asset('static/index')}}/demo/faces/female/1.jpg)"></span>
-                      <div>
-                        <strong>Alice</strong> started new task: Tabler UI design.
-                        <div class="small text-muted">1 hour ago</div>
-                      </div>
-                    </a>
-                    <a href="#" class="dropdown-item d-flex">
-                      <span class="avatar mr-3 align-self-center" style="background-image: url({{asset('static/index')}}/demo/faces/female/18.jpg)"></span>
-                      <div>
-                        <strong>Rose</strong> deployed new version of NodeJS REST Api V3
-                        <div class="small text-muted">2 hours ago</div>
-                      </div>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="#" class="dropdown-item text-center">Mark all as read</a>
-                  </div>
-                </div> -->
-                
                 @if (Route::has('login'))
                     @auth
                     <div class="dropdown">
