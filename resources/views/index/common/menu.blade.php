241b7c5c74b332f21e284b70eb972561ec3c0719
Kothing <39289309+kothing@users.noreply.github.com>
2021-01-24 14:51:58 +0800

    Update menu.blade.php

--- resources/views/index/common/menu.blade.php
+++ resources/views/index/common/menu.blade.php
@@ -1,7 +1,7 @@
 <div class="header collapse d-lg-flex p-0" id="headerMenuCollapse">
   <div class="container">
     <div class="row align-items-center">
-      <div class="col-lg order-lg-first">
+      <div class="col-lg-8 order-lg-first">
         <ul class="nav nav-tabs border-0 flex-column flex-lg-row">
           <li class="nav-item">
             <a href="{{ url('/') }}" style="color:black" class="nav-link"><i class="fe fe-home"></i> 首页</a>
@@ -19,6 +19,9 @@
             <a href="{{ url('/home') }}" style="color:black" class="nav-link"><i class="fe fe-edit"></i> 随心笔记</a>
           </li>
         </ul>
+      </div>
+      <div class="col-lg-4 weather-box">
+        <iframe allowtransparency="true" frameborder="0" width="317" height="28" scrolling="no" src="//tianqi.2345.com/plugin/widget/index.htm?s=3&z=1&t=1&v=0&d=1&bd=0&k=&f=&ltf=009944&htf=cc0000&q=1&e=0&a=1&c=54511&w=317&h=28&align=left"></iframe>
       </div>
     </div>
   </div>
